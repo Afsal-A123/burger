2cdac540a8a5518d0f2a84352e59cd4a3519e6bc
Afsal-A123 <afsalabbas805@gmail.com>
2025-01-06 20:29:43 +0530

    about initiated

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,8 +1,22 @@
+import about1 from '../../asserts/about1.png';
+import "./About.css";
 function About()
 {
     return(
-        <div>
-            <h1>Welcome to The Burger King</h1>
+        <div className='about'>
+            <div>
+
+            <img src={about1} alt='about'/>
+            </div>
+            <div className='desc'>
+                <h1>Nothing brings people together like a good burger </h1>
+                <p>Semper lacus cursus porta primis ligula risus tempus and sagittis ipsum mauris lectus laoreet purus ipsum tempor enim ipsum porta justo integer ultrice aligula lectus aenean magna and pulvinar purus at pretium gravida</p>
+                <ul>
+                    <li>Fringilla risus, luctus mauris orci auctor purus euismod pretium purus pretium ligula rutrum tempor sapien</li>
+                    <li>Quaerat sodales sapien euismod purus blandit</li>
+                    <li>Nemo ipsam egestas volute turpis dolores ut aliquam quaerat sodales sapien undo pretium a purus mauris</li>
+                </ul>
+            </div>
         </div>
     )
 }
