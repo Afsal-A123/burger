01bdc1a9eb18c256a663d1c260ac46297b029887
Bhuwanesh16 <bhuwaneshmanivel@gmail.com>
2025-01-06 21:53:22 +0530

    Navigation

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import About from './Components/About/About';
 import Menu from './Components/Menu/Menu';
 import Home from './Components/home/Home';
-import data from './Burger.json'; // Import the correct data from JSON file
+import data from './Burger.json'; 
 
 function App() {
   return (
@@ -13,11 +13,12 @@ function App() {
       <BrowserRouter>
         <NavigationBar />
         <Routes>
-          <Route path="/Home" element={<Home />} />
+         
+      <Route path="/Home" element={<Home />} />
           <Route path="/Menu" element={<Menu products={data.products} />} /> {/* Pass the products array to Menu */}
           <Route path="/About" element={<About />} />
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> 
     </div>
   );
 }
--- src/Components/header/NavigationBar.js
+++ src/Components/header/NavigationBar.js
@@ -2,41 +2,48 @@ import React from 'react';
 import Logo from '../../asserts/Logo.png';
 import '../header/NavigationBar.css';
 import { Button } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
 export default function NavigationBar() {
+  const navigate = useNavigate();
+
+  const navigateTo = (path) => {
+    navigate(path, { replace: true }); // Navigate without adding to history
+  };
+
   return (
     <header className="nav">
       <div className="container-fluid">
-        
         <div className="part-1">
           <img src={Logo} alt="Company Logo" height={100} width={100} />
         </div>
 
         <div className="part-2">
           <nav>
-            <ul >
+            <ul>
               <li className="list-inline-item">
-                <Button><Link to="/Home">Home</Link></Button>
+                <Button onClick={() => navigateTo('/Home')}>Home</Button>
               </li>
               <li className="list-inline-item">
-                <Button><Link to="/Menu">Menu</Link></Button>
+                <Button onClick={() => navigateTo('/Menu')}>Menu</Button>
               </li>
               <li className="list-inline-item">
-                <Button><Link to="/Contact">Contact</Link></Button>
+                <Button onClick={() => navigateTo('/Contact')}>Contact</Button>
               </li>
               <li className="list-inline-item">
-                <Button><Link to="/About">About Us</Link></Button>
+                <Button onClick={() => navigateTo('/About')}>About Us</Button>
               </li>
             </ul>
           </nav>
         </div>
 
-        <div className="part-3">
-        </div>
-          <Button variant="outlined" color="tertiary">Login</Button>
-          <Button variant="contained" color="info">
-            Sign Up
-          </Button>
+        <div className="part-3"></div>
+        <Button variant="outlined" color="tertiary">
+          Login
+        </Button>
+        <Button variant="contained" color="info">
+          Sign Up
+        </Button>
       </div>
     </header>
   );
